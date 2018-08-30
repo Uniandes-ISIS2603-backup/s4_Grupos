f2e07977907606843910cdd2b4b486089e0c3320
estudiante <estudiante@ISIS2603-328.sis.virtual.uniandes.edu.co>
2018-08-30 17:53:45 -0500

    Arreglo de package

--- s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/resources/NoticiaResourse.java
+++ s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/resources/NoticiaResourse.java
@@ -9,7 +9,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import co.uniandes.csw.grupos.dtos.NoticiaDTO;
+import co.edu.uniandes.csw.grupos.dtos.NoticiaDTO;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
