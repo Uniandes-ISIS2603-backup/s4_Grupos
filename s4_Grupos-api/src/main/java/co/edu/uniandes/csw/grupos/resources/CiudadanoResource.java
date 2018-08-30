5be8780e778cb3b298bca6d95c209876ea224ec2
DanielRamirez <da.ramirez14@ISIS2603-328.sis.virtual.uniandes.edu.co>
2018-08-30 10:31:45 -0500

    Creación de las clases de los subrecursos

--- s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/resources/CiudadanoResource.java
+++ s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/resources/CiudadanoResource.java
@@ -6,7 +6,8 @@
 package co.edu.uniandes.csw.grupos.resources;
 
 import co.edu.uniandes.csw.grupos.dtos.CiudadanoDTO;
-import java.util.ArrayList;
+import co.edu.uniandes.csw.grupos.exceptions.BusinessLogicException;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
@@ -15,6 +16,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -47,9 +49,10 @@ public class CiudadanoResource
      * @return la información de un ciudadano
      */
     @GET
-    public void consultaUnCiudadano()
+    @PathParam("(ciudadanosId: \\d++)")
+    public List<CiudadanoDTO> consultaDeCiudadanos(@PathParam("ciudadanosId") List<CiudadanoDTO> ciudadanos) throws BusinessLogicException
     {
-        
+        return ciudadanos;
     }
     
     /**
