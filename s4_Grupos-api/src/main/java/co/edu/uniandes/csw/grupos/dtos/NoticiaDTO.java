ef10c9e7e830412017040b6c2898a097f819182d
estudiante <estudiante@ISIS2603-328.sis.virtual.uniandes.edu.co>
2018-08-30 17:55:39 -0500

    Arreglo de package, habia un package de más con nombre co.uniandes.csw.grupos.dtos

--- s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/dtos/NoticiaDTO.java
+++ s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/dtos/NoticiaDTO.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.uniandes.csw.grupos.dtos;
+package co.edu.uniandes.csw.grupos.dtos;
+import co.uniandes.csw.grupos.dtos.*;
 import java.io.Serializable;
 import co.edu.uniandes.csw.grupos.entities.NoticiaEntity;
 
