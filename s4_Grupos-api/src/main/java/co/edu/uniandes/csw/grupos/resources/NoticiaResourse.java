354f954f839624eebe9daffcb8c6eeba3793d196
estudiante <estudiante@ISIS2603-321.sis.virtual.uniandes.edu.co>
2018-08-30 10:28:28 -0500

    DTO distrito y recurso

--- s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/resources/NoticiaResourse.java
+++ s4_Grupos-api/src/main/java/co/edu/uniandes/csw/grupos/resources/NoticiaResourse.java
@@ -9,7 +9,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import co.uniandes.csw.grupos.dtos.NoticiaDTO;
+import co.edu.uniandes.csw.grupos.dtos.NoticiaDTO;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -21,39 +21,43 @@ import javax.ws.rs.PathParam;
  * @author ISIS2603
  * @version 1.0
  */
-@Path("noticias")
 @Produces("application/json")
 @Consumes("application/json")
 @RequestScoped
 public class NoticiaResourse {
    
     @POST
-    public NoticiaDTO createNoticia(NoticiaDTO noticia)
+    public NoticiaDTO createNoticia(@PathParam("gruposId") Long gruposID,NoticiaDTO noticia)
     {
         return noticia;
     }
     
     @GET
+    public NoticiaDTO consultarNoticia(@PathParam("gruposId") Long gruposID)
+    {
+        return new NoticiaDTO();
+    }
+    @GET
     @Path("{id:\\d+`}")
-    public NoticiaDTO consultarNoticia(@PathParam("id") Long id)
+    public NoticiaDTO consultarNoticia(@PathParam("gruposId") Long gruposID,@PathParam("id") Long id)
     {
         return new NoticiaDTO();
     }
     @PUT
      @Path("{id:\\d+`}")
-    public NoticiaDTO editarNoticia(@PathParam("id") Long id)
+    public NoticiaDTO editarNoticia(@PathParam("gruposId") Long gruposID,@PathParam("id") Long id)
     {
         return new NoticiaDTO();
     }
     /**
-     * Borra la noticia con el id asociado recibido en la URL.
-     *
+     * Borra la noticia del grupo don el id con el id asociado recibido en la URL.
+     * *@param gruposId El ID del grupo del cual se va a eliminar la noticia.
      * @param id Identificador de la editorial que se desea borrar.
      * Este debe ser una cadena de dígitos.
      */
     @DELETE
     @Path("{id: \\d+}")
-    public void deleteEditorial(@PathParam("id") Long id) {
+    public void deleteEditorial(@PathParam("gruposId") Long gruposID,@PathParam("id") Long id) {
        
     }
 }
