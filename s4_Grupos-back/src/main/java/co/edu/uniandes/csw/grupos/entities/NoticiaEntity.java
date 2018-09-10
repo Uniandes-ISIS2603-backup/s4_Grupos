6d5dea6cb484d7dbc23645f0350e403c97613cfe
sarabepu <sm.bejarano44@uniandes.edu.co>
2018-09-10 11:32:59 -0500

    NoticiaEntity sin la relacion con grupo comentada

--- s4_Grupos-back/src/main/java/co/edu/uniandes/csw/grupos/entities/NoticiaEntity.java
+++ s4_Grupos-back/src/main/java/co/edu/uniandes/csw/grupos/entities/NoticiaEntity.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import uk.co.jemos.podam.common.PodamExclude;
 /**
@@ -19,10 +20,10 @@ import uk.co.jemos.podam.common.PodamExclude;
 public class NoticiaEntity extends BaseEntity implements Serializable{
     private String descripcion;
 private String rutaImagen;
-/**@PodamExclude
+@PodamExclude
 @ManyToOne(cascade = CascadeType.ALL)
-private GruposInteresEntity grupo;
-**/
+private GrupoDeInteresEntity grupo;
+
 @PodamExclude
     @OneToMany(mappedBy = "noticia", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<ComentarioEntity> comentarios = new ArrayList<ComentarioEntity>();
@@ -39,7 +40,7 @@ public NoticiaEntity()
     public void setComentarios(List<ComentarioEntity> comentarios) {
         this.comentarios = comentarios;
     }
- /**
+
 public GrupoDeInteresEntity getGrupoDeInteres() {
         return grupo;
     }
@@ -47,7 +48,7 @@ public GrupoDeInteresEntity getGrupoDeInteres() {
     public void setGrupoDeInteres(GrupoDeInteresEntity grupo) {
         this.grupo = grupo;
     }
-    * */
+  
     public String getDescripcion() {
         return descripcion;
     }
